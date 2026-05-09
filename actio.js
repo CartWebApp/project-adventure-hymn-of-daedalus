2f07e834f1c356d10b93edfadac38e524ea2e2c2
Doomfangre <Doomfangre@gmail.com>
2026-05-08 22:54:17 -0700

    I'm done with ts

---
+++ actio.js
@@ -0,0 +1,225 @@
+function goneagainO() {
+    document.getElementById("option1").style.visibility = "hidden";
+    document.getElementById("option2").style.visibility = "hidden";
+}
+
+let health = 9;
+
+
+
+function quick1() {
+    document.getElementById("qte1").style.transform = `translate(0px, 0px)`;
+    document.getElementById("qte1").style.backgroundImage = `url(qte1.gif)`;
+    const randomer = Math.floor(Math.random() * 700) + 100;
+    const randomest = Math.floor(Math.random() * 550) + 25;
+    document.getElementById("qte1").style.transform = `translate(${randomer}px, ${randomest}px)`;
+    document.getElementById("qte1").style.visibility = `visible`;
+    document.addEventListener('keyup', (event) => {
+    if (event.key === 'w') {
+        document.getElementById("qte1").style.visibility = "hidden";
+        document.getElementById("qte1").style.backgroundImage = ``;
+        health -= 1;
+
+
+        if (health == 8) {
+        document.getElementById("h9").style.visibility = `hidden`;
+        } else if (health == 7) {
+        document.getElementById("h8").style.visibility = `hidden`;
+        } else if (health == 6) {
+        document.getElementById("h7").style.visibility = `hidden`;
+        } else if (health == 5) {
+        document.getElementById("h6").style.visibility = `hidden`;
+        } else if (health == 4) {
+        document.getElementById("h5").style.visibility = `hidden`;
+        } else if (health == 3) {
+        document.getElementById("h4").style.visibility = `hidden`;
+        } else if (health == 2) {
+        document.getElementById("h3").style.visibility = `hidden`;
+        } else if (health == 1) {
+        document.getElementById("h2").style.visibility = `hidden`;
+        } else if (health == 0) {
+        document.getElementById("h1").style.visibility = `hidden`;
+        window.location.href = "act2.html";
+        }
+
+        return;
+    }
+}, { once: true }); 
+
+setTimeout(() => {
+        document.getElementById("qte1").style.visibility = "hidden";
+        document.getElementById("qte1").style.backgroundImage = ``;
+    }, 900);
+};
+
+function quick2() {
+    document.getElementById("qte2").style.transform = `translate(0px, 0px)`;
+    document.getElementById("qte2").style.backgroundImage = `url(qte2.gif)`;
+    const randomer2 = Math.floor(Math.random() * 700) + 100;
+    const randomest2 = Math.floor(Math.random() * 550) + 25;
+    document.getElementById("qte2").style.transform = `translate(${randomer2}px, ${randomest2}px)`;
+    document.getElementById("qte2").style.visibility = `visible`;
+    document.addEventListener('keyup', (event) => {
+    if (event.key === 'a') {
+        document.getElementById("qte2").style.visibility = "hidden";
+        document.getElementById("qte2").style.backgroundImage = ``;
+        health -= 1;
+
+
+        if (health == 8) {
+        document.getElementById("h9").style.visibility = `hidden`;
+        } else if (health == 7) {
+        document.getElementById("h8").style.visibility = `hidden`;
+        } else if (health == 6) {
+        document.getElementById("h7").style.visibility = `hidden`;
+        } else if (health == 5) {
+        document.getElementById("h6").style.visibility = `hidden`;
+        } else if (health == 4) {
+        document.getElementById("h5").style.visibility = `hidden`;
+        } else if (health == 3) {
+        document.getElementById("h4").style.visibility = `hidden`;
+        } else if (health == 2) {
+        document.getElementById("h3").style.visibility = `hidden`;
+        } else if (health == 1) {
+        document.getElementById("h2").style.visibility = `hidden`;
+        } else if (health == 0) {
+        document.getElementById("h1").style.visibility = `hidden`;
+        window.location.href = "act2.html";
+        }
+
+        return;
+    }
+}, { once: true }); 
+
+    setTimeout(() => {
+        document.getElementById("qte2").style.visibility = "hidden";
+        document.getElementById("qte2").style.backgroundImage = ``;
+    }, 900);
+};
+
+function quick3() {
+    document.getElementById("qte3").style.transform = `translate(0px, 0px)`;
+    document.getElementById("qte3").style.backgroundImage = `url(qte3.gif)`;
+    const randomer3 = Math.floor(Math.random() * 700) + 100;
+    const randomest3 = Math.floor(Math.random() * 550) + 25;
+    document.getElementById("qte3").style.transform = `translate(${randomer3}px, ${randomest3}px)`;
+    document.getElementById("qte3").style.visibility = `visible`;
+    document.addEventListener('keyup', (event) => {
+    if (event.key === 'p') {
+        document.getElementById("qte3").style.visibility = "hidden";
+        document.getElementById("qte3").style.backgroundImage = ``;
+        health -= 1;
+        
+        if (health == 8) {
+        document.getElementById("h9").style.visibility = `hidden`;
+        controller.abort();
+        return;
+        } else if (health == 7) {
+        document.getElementById("h8").style.visibility = `hidden`;
+        return;
+        } else if (health == 6) {
+        document.getElementById("h7").style.visibility = `hidden`;
+        return;
+        } else if (health == 5) {
+        document.getElementById("h6").style.visibility = `hidden`;
+        return;
+        } else if (health == 4) {
+        document.getElementById("h5").style.visibility = `hidden`;
+        return;
+        } else if (health == 3) {
+        document.getElementById("h4").style.visibility = `hidden`;
+        return;
+        } else if (health == 2) {
+        document.getElementById("h3").style.visibility = `hidden`;
+        return;
+        } else if (health == 1) {
+        document.getElementById("h2").style.visibility = `hidden`;
+        return;
+        } else if (health == 0) {
+        document.getElementById("h1").style.visibility = `hidden`;
+        window.location.href = "act2.html";
+        return;
+        }
+
+        return;
+    }
+}, { once: true }); 
+    setTimeout(() => {
+        document.getElementById("qte3").style.visibility = "hidden";
+        document.getElementById("qte3").style.backgroundImage = ``;
+    }, 900);
+};
+
+function wait() {
+    setTimeout(() => {
+        document.getElementById("option1").style.visibility = "visible";
+        document.getElementById("option2").style.visibility = "visible";
+    }, 2500);
+}
+
+
+let aura = 50;
+let tro = aura + "";
+
+
+function quickattack() {
+    quick1();
+    quick2();
+    quick3();
+    wait();
+
+    
+    setTimeout(() => {
+        setTimeout(() => {
+            setTimeout(() => {
+                aura = aura + 25;
+            let tro = aura + "";
+            document.getElementById("Aura").value = tro;
+                if (document.getElementById("1h").style.visibility == `hidden`) {
+                    if (document.getElementById("2h").style.visibility == `hidden`) {
+                        if (document.getElementById("3h").style.visibility == 'hidden') {
+
+                        }
+                        document.getElementById("3h").style.visibility = 'hidden';
+                        window.location.href = "over.html"; 
+                    }
+                    document.getElementById("2h").style.visibility = `hidden`;
+                
+                }  else {
+                   document.getElementById("1h").style.visibility = `hidden`; 
+                    
+                } 
+        }, 900);
+        }, 900);
+    }, 900);
+}
+
+
+function defending() {
+    
+     if (document.getElementById("2h").style.visibility == `hidden`) {
+                    if (document.getElementById("1h").style.visibility == `hidden`) {
+                        document.getElementById("1h").style.visibility = `visible`;
+                    }
+                    document.getElementById("2h").style.visibility = `visible`;
+                }  else {
+                   document.getElementById("1h").style.visibility = `visible`; 
+                }
+    wait();
+
+    setTimeout(() => {
+        setTimeout(() => {
+            setTimeout(() => {
+                document.getElementById("collapse").style.animation = ``;
+        }, 900);
+        }, 900);
+    }, 100);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+document.getElementById("option1").addEventListener("click", goneagainO);
+document.getElementById("option1").addEventListener("click", quickattack);
+
+document.getElementById("option2").addEventListener("click", goneagainO);
+document.getElementById("option2").addEventListener("click", defending);
